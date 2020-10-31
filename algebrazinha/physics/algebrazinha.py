4d9a9b57e5e10f3b2a6c3e384015b9e70a8165d4
William <william.h.sumida@gmail.com>
2020-10-31 16:04:31 -0300

    changed the sum_vector parameter to a list of vectors instead of 2 vectors

--- algebrazinha/physics/algebrazinha.py
+++ algebrazinha/physics/algebrazinha.py
@@ -1,8 +1,12 @@
 from vector import Vector
 class Algebrazinha:
 
-    def sum_vectors(self, v1, v2):
-        x = v1.x + v2.x
-        y = v1.y + v2.y
-        z = v1.z + v2.z
+    def sum_vectors(self, vectors):
+        x=0
+        y=0
+        z=0
+        for vector in vectors:
+            x += vector.x
+            y += vector.y
+            z += vector.z
         return Vector(x,y,z)
--- algebrazinha/physics/runner.py
+++ algebrazinha/physics/runner.py
@@ -4,7 +4,9 @@ from vector import Vector
 v1 = Vector(1.0, 2.3)
 v2 = Vector(5.0, 3)
 
-alg = Algebrazinha()
+algebrazinha = Algebrazinha()
 
-v_res = alg.sum_vectors(v1,v2)
+v_res = algebrazinha.sum_vectors([v1,v2])
+print(v1.x, v1.y)
+print(v2.x, v2.y)
 print(v_res.x, v_res.y)
